86fba277221c6e8ff99fdf86e95a2f8e63459c70
Lmnoppy <grqmclean@gmail.com>
2017-01-28 13:03:01 +0000

    Bug fixes and other small problems

--- README.md
+++ README.md
@@ -4,3 +4,42 @@ Scrippy is a Chrome and Firefox browser extension that stores SQL statments (mor
 Currently the JSON payload files are the only part that is open source and free for everyone to do as they wish with them, this is due to the extension still in development and not ready to be made public. However, you can help by adding payloads to the...well payload files :) 
 
 This tool is made for web developers and pen testers that want to test sites/have legal authorisation to work on for SQL and XSS injections (and what other types of attacks get implemented into this tool). I will not be held responsible (and  for your at all in attacking websites that your not allowed and on top of that if this tool fucks up and attacks sites randomly then again i take no responsibility. 
+
+# How to use Scrippy
+
+
+# To do
+
+Documentation:          Start up                                                -
+                        How to use                                              -
+                        Examples                                                -
+                        
+
+Refactor code:          Core code base needs to be refactored 
+
+SQL:                    Right click -> SQL -> Quick predefinded statements      - Done (although need to change the statments to more usful statements)
+                        Right click -> SQL -> Favorite statements               - Done
+                                Set Favorites in options/popup menu page        - Done
+                        Click popup menu -> SQL payloads -> list of Payloads    - Done (More paylods need to be added) 
+                        
+XSS:                    Right click -> XSS -> Quick predefinded statements      - What lanuage? JS?
+                        Right click -> XSS -> Favorite statements               - 
+                                Set Favorites in options/popup menu page        - 
+                        Click popup menu -> SQL payloads -> list of Payloads    -
+
+
+Encoding:       Select text -> right click it -> encode -> URL, Base 64, double encode
+                                Right click menu                                - Done
+                                URL encoding                                    - 
+                                Base 64                                         -
+                                Double                                          - 
+                        
+SQL Map intergration:   Right click on input -> SQL Map here -> open SQL mapper - With current element and basic command inputed.
+                                Right click menu                                - Done
+                                get current element                             -
+                                Open SQL mapper                                 -
+                                input command                                   -
+                                
+
+
+                                
--- content/content.js
+++ content/content.js
@@ -4,7 +4,6 @@
     *Var for element that user clicked
     *Vars for menu and the selected strings/payloads
 ***************************************************************************/
-
 //element clicked on the current tab.
 var clickedEl = null;
 
@@ -19,8 +18,6 @@ var quickSQL7 = "Some sort of string here";
 var quickSQL8 = "Some sort of string here";
 var quickSQL9 = "Some sort of string here";
 var quickSQL10 = "Some sort of string here";
-
-
 /**********Fav SQL**********/
 var fav1 = inputValues();
 var fav2 = inputValues();
@@ -34,24 +31,23 @@ var fav5 = inputValues();
  *Input function gets the input values from the options
  *and sets the global vars to equal the items in the chrome.storage
  ***************************************************************************/
-
 // Restores values stored in storage.
 function inputValues() {
     browser.storage.local.get({
-            storedMyFav1: '',
-            storedMyFav2: '',
-            storedMyFav3: '',
-            storedMyFav4: '',
-            storedMyFav5: '',
-        },
-        //function that places the values stored in chrome.storage and puts them in the input boxes
+            storedMyFav1: ''
+            , storedMyFav2: ''
+            , storedMyFav3: ''
+            , storedMyFav4: ''
+            , storedMyFav5: ''
+        , }
+        , //function that places the values stored in chrome.storage and puts them in the input boxes
         function (items) {
             fav1 = items.storedMyFav1;
             fav2 = items.storedMyFav2;
             fav3 = items.storedMyFav3;
             fav4 = items.storedMyFav4;
             fav5 = items.storedMyFav5;
-    });
+        });
 }
 
 /***************************************************************************
@@ -59,16 +55,14 @@ function inputValues() {
     
     *on mouse click update clicked element
 ***************************************************************************/
-
 // listener to determine what element the user right clicked on
-document.addEventListener("mousedown", function(event) {
+document.addEventListener("mousedown", function (event) {
     //right click
-    if(event.button == 2) { 
+    if (event.button == 2) {
         clickedEl = event.target.id;
         console.log("the event target is " + event.target + " The classname is " + clickedEl);
     }
 }, true);
-
 
 /***************************************************************************
     ON MESSANGER LISTENER
@@ -76,192 +70,186 @@ document.addEventListener("mousedown", function(event) {
     *Listens for message to be recived - on doing so checks menu 
      and fires correct menu var
 ***************************************************************************/
-
-
 //Context Menu ID that was clicked
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.menuId == "20") {
-        
         //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
+        sendResponse({
+            gotIt: "Got it"
+        });
         //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL1;
-        
-    } else if (request.menuId == "21") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL2;
-        
-    } else if (request.menuId == "22") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL3;
-        
-    }  else if (request.menuId == "23") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL4;
-        
-    }  else if (request.menuId == "24") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-  
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL5;
-        
-    }  else if (request.menuId == "25") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-       
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL6;
-        
-    }  else if (request.menuId == "26") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL7;
-        
-    }  else if (request.menuId == "27") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL8;
-        
-    }  else if (request.menuId == "28") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL9;
-        
-    } else if (request.menuId == "29") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-  
-        //insert string on current element
-        document.getElementById(clickedEl).value= quickSQL10;
-        
-    }  else if (request.menuId == "30") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav1;
-        
-    }  else if (request.menuId == "31") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav2;
-        
-    }  else if (request.menuId == "32") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav3;
-        
-    }  else if (request.menuId == "33") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav4;
-        
-    } 
-     else if (request.menuId == "34") {
-         
-         //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav5;
-        
-    }  else if (request.menuId == "35") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav6;
-        
-    }  else if (request.menuId == "36") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav7;
-        
-    }  else if (request.menuId == "37") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav8;
-        
-    }  else if (request.menuId == "38") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav9;
-        
-    }  else if (request.menuId == "39") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav10;
-        
-    }  else if (request.menuId == "40") {
-        
-        //Send response to background js
-        sendResponse({gotIt: "Got it"});
-        
-        //insert string on current element
-        document.getElementById(clickedEl).value= fav1;
-        
-    } else {
-        sendResponse({something: "Dont got it"});
+        document.getElementById(clickedEl).value = quickSQL1;
     }
-    
+    else if (request.menuId == "21") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL2;
+    }
+    else if (request.menuId == "22") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL3;
+    }
+    else if (request.menuId == "23") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL4;
+    }
+    else if (request.menuId == "24") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL5;
+    }
+    else if (request.menuId == "25") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL6;
+    }
+    else if (request.menuId == "26") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL7;
+    }
+    else if (request.menuId == "27") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL8;
+    }
+    else if (request.menuId == "28") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL9;
+    }
+    else if (request.menuId == "29") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = quickSQL10;
+    }
+    else if (request.menuId == "30") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav1;
+    }
+    else if (request.menuId == "31") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav2;
+    }
+    else if (request.menuId == "32") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav3;
+    }
+    else if (request.menuId == "33") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav4;
+    }
+    else if (request.menuId == "34") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav5;
+    }
+    else if (request.menuId == "35") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav6;
+    }
+    else if (request.menuId == "36") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav7;
+    }
+    else if (request.menuId == "37") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav8;
+    }
+    else if (request.menuId == "38") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav9;
+    }
+    else if (request.menuId == "39") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav10;
+    }
+    else if (request.menuId == "40") {
+        //Send response to background js
+        sendResponse({
+            gotIt: "Got it"
+        });
+        //insert string on current element
+        document.getElementById(clickedEl).value = fav1;
+    }
+    else {
+        sendResponse({
+            something: "Dont got it"
+        });
+    }
 });
-
-
-
-
 
 /***************************************************************************
  * ENCODE TO URL ENCODING STANDARD
@@ -270,6 +258,3 @@ browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
  *      paste over highlighted string
  *      
  * ***************************************************************************/
-
-
-
