5f40a974ba8215f0a4442f3f305517d9a5990726
Lmnoppy <code@Lmnoppy.com>
2018-05-12 22:19:58 +0100

    new version

---
+++ Plugins/Firefox-V2/src/app/background/background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.scss']
+})
+export class BackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Plugins/Firefox-V2/src/app/options/options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-options',
+  templateUrl: './options.component.html',
+  styleUrls: ['./options.component.scss']
+})
+export class OptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
