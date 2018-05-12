53a7190014bd0b8a5c7d4c1460fa6396aa399995
Lmnoppy <code@Lmnoppy.com>
2018-05-12 21:46:04 +0100

    updated legal information message

--- Plugins/Firefox-X2/src/app/app-routing.module.ts
+++ Plugins/Firefox-X2/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {PopupComponent} from "./popup/popup.component";
 import {EventpageComponent} from "./eventpage/eventpage.component";
+import {OptionsComponent} from "./options/options.component";
 
 const routes: Routes = [
   { path: 'homepage', component: HomepageComponent },
   { path: 'event-page', component: EventpageComponent },
   { path: 'popup', component: PopupComponent },
+  { path: 'options', component: OptionsComponent },
   { path: '', redirectTo: 'homepage', pathMatch: 'full' }
 ];
 
