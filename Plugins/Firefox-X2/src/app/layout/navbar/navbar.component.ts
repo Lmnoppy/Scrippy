6ac3a1e8b9ac62132bc9dbd2330d5eb16af8cd57
Lmnoppy <code@Lmnoppy.com>
2018-05-13 08:41:30 +0100

    removed old attempt

--- Plugins/Firefox-X2/src/app/layout/navbar/navbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-layout-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
-})
-export class NavbarComponent {
-}
